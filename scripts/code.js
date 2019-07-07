c8c076dc1a60800eb88413adb568bf3b221fe088
Tomasz Nowak <tn@mpro.pl>
2019-07-07 22:44:55 +0200

    generator liczb

---
+++ scripts/code.js
@@ -0,0 +1,12 @@
+function getlotto(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+  }
+for(var i = 0; i < 6;i++){
+  var numery = getlotto(1,49)
+  document.getElementById('container').innerHTML += '<div class="result">' + (numery) + '</div>';
+
+}
+
